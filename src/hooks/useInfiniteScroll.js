cda701df896eae21b954132419b8089d4d1eeb46
Rzanklod <malwinaxxx666@gmail.com>
2025-05-17 16:20:21 +0200

    Added custom infinite scroll hook that supports pagination via pages and timestamp

---
+++ src/hooks/useInfiniteScroll.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+
+const debounce = (func, delay) => {
+  let timeoutId;
+  return function (...args){
+    if(timeoutId){
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  }
+}
+
+const useInfiniteScroll = ({
+  fetchData,
+  useTimestamp = false,
+  initialPage = 1,
+  getLastTimestamp,
+}) => {
+  const [page, setPage] = useState(initialPage);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const loadData = async () => {
+    setIsFetching(true);
+    if(useTimestamp){
+      const timestamp = getLastTimestamp?.();
+      await fetchData(timestamp);
+    } else {
+      const nextPage = page + 1;
+      await fetchData(nextPage);
+      setPage(nextPage);
+    }
+    setIsFetching(false);
+  }
+
+  useEffect(() => {
+    const init = async () => {
+      await loadData();
+    }
+
+    init();
+  }, []);
+
+  const handleScroll = debounce(async () => {
+    const bottom =
+      Math.ceil(window.innerHeight + window.scrollY) >=
+      document.documentElement.scrollHeight - 200;
+    if(bottom && !isFetching){
+      await loadData();
+    }
+  }, 200);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [isFetching]);
+
+  return { isFetching, page }
+}
+
+export default useInfiniteScroll;
