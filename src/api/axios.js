06ed3912e53db6314bccce745ca65a5b2d0c7882
Rzanklod <malwinaxxx666@gmail.com>
2025-06-27 19:57:38 +0200

    fix workflow

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/api/v1`
 
+console.log(BASE_URL);
+
 export default axios.create({
     baseURL: BASE_URL,
 })
